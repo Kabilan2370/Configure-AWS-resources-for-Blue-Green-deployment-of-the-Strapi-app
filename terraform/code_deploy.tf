36a1e85375c45f54587d52190dbe9aa791955928
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-24 00:57:20 +0530

    Update code_deploy.tf

--- terraform/code_deploy.tf
+++ terraform/code_deploy.tf
@@ -70,19 +70,15 @@ resource "aws_codedeploy_deployment_group" "ecs_deployment_group" {
   }
 
   blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+  }
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
       termination_wait_time_in_minutes = 5
     }
-
-  deployment_ready_option {
-    action_on_timeout = "CONTINUE_DEPLOYMENT"
-}
-
     
-    
-}
-
+  }
 
 }
 
