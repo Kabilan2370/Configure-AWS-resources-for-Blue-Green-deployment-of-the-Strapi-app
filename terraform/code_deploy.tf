0d8c545d821be5bb41b8c2e6067eb0eb61160515
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-26 13:46:57 +0530

    Update code_deploy.tf

--- terraform/code_deploy.tf
+++ terraform/code_deploy.tf
@@ -1,69 +1,45 @@
-# iam role for code deploy
+# CodeDeploy Application
+resource "aws_codedeploy_app" "strapi" {
+  compute_platform = "ECS"
+  name             = "strapi-app-deploy"
+}
+
+# IAM Role for CodeDeploy
 resource "aws_iam_role" "codedeploy_role" {
-  name = "ecs-codedeploy-docker-strapi"
+  name = "strapi-codedeploy-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "codedeploy.amazonaws.com"
-        }
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+# Attach the managed policy
+resource "aws_iam_role_policy_attachment" "codedeploy" {
   role       = aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
 
-# code deploy application
-resource "aws_codedeploy_app" "ecs_app" {
-  name = "ecs-docker-strapi-deploy"
-  compute_platform = "ECS"
-}
-
-# code deployment group
-resource "aws_codedeploy_deployment_group" "ecs_deployment_group" {
-  app_name              = aws_codedeploy_app.ecs_app.name
-  deployment_group_name = "ecs-strapi-dg"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
-
-  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+# Deployment Group
+resource "aws_codedeploy_deployment_group" "strapi" {
+  app_name               = aws_codedeploy_app.strapi.name
+  deployment_group_name  = "strapi-deploy-group"
+  service_role_arn       = aws_iam_role.codedeploy_role.arn
   
-
-  ecs_service {
-    cluster_name = aws_ecs_cluster.cluster.name
-    service_name = aws_ecs_service.service.name
-  }
-
+  # Use the correct ECS deployment configuration
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"  # Can also use ECSCanary10Percent5Minutes
+  
   deployment_style {
     deployment_type   = "BLUE_GREEN"
     deployment_option = "WITH_TRAFFIC_CONTROL"
   }
 
-  load_balancer_info {
-    target_group_pair_info {
-      target_group {
-        name = aws_lb_target_group.blue_strapi.name
-      }
-      target_group {
-        name = aws_lb_target_group.green_strapi.name
-      }
-
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.http.arn]
-      }
-
-    
-    }
-  }
-
-  # Enable automatic rollback on failure
   auto_rollback_configuration {
     enabled = true
     events  = ["DEPLOYMENT_FAILURE"]
@@ -71,15 +47,34 @@ resource "aws_codedeploy_deployment_group" "ecs_deployment_group" {
 
   blue_green_deployment_config {
     deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-  }
+      action_on_timeout    = "CONTINUE_DEPLOYMENT"
+      wait_time_in_minutes = 0
+    }
+
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
       termination_wait_time_in_minutes = 5
     }
-    
   }
 
+  ecs_service {
+    cluster_name = aws_ecs_cluster.strapi_cluster.name
+    service_name = aws_ecs_service.strapi_service.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.http_80.arn]
+      }
+
+      target_group {
+        name = aws_lb_target_group.blue_tg.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.green_tg.name
+      }
+    }
+  }
 }
-
-
